2df6f586ec64e3d20f561f66051289ef68be46cc
stevenjlance <44912347+stevenjlance@users.noreply.github.com>
2021-11-01 15:55:22 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./styles.css";
+import React from "react";
+
+// Import your components and data here
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      {/* Code your components and  other JSX elements below this*/}
+    </div>
+  );
+}
+
+export default App;
